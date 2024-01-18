554e60d56f072620490b10cef610336f78bc5279
affantaufiqur <affantaufiqur@gmail.com>
2024-01-18 13:46:58 +0700

    move to product to old

--- schema/cart.ts
+++ schema/cart.ts
@@ -1,6 +1,10 @@
-import { object, string, number, boolean, minLength, integer, toMinValue } from "valibot";
+import { object, number, integer, toMinValue } from "valibot";
 
 export const cartSchema = object({
   id: number([integer(), toMinValue(1)]),
   quantity: number([integer()]),
 });
+
+// const testRegex = object({
+//     name: string([regex(/[a-z]/g, "hello")]),
+// })
